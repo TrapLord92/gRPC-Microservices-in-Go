c2806bb14ccb88924385ca782bf75e3bafdeaf3b
TrapLord92 <traplord345@gmail.com>
2024-10-15 03:58:47 +0500

    update

---
+++ ClientandServerSide/cmd/grpc_client/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	desc "github.com/TrapLord92/gRPC-Microservices-in-Go/pkg/note_v1"
+	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	address = "localhost:50051"
+	noteID  = 12
+)
+
+func main() {
+	// Create a new gRPC client connection using NewClient
+	conn, err := grpc.NewClient("localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials())) // Insecure credentials for non-TLS
+	if err != nil {
+		log.Fatalf("Failed to connect to server: %v", err)
+	}
+	defer conn.Close()
+
+	c := desc.NewNoteV1Client(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Get(ctx, &desc.GetRequest{Id: noteID})
+	if err != nil {
+		log.Fatalf("failed to get note by id: %v", err)
+	}
+
+	log.Printf(color.RedString("Note info:\n"), color.GreenString("%+v", r.GetNote()))
+}
